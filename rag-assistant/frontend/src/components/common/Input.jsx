21ebffcbf2b98bcbb04d8d51176ab0bca480fefb
Ramesh Murala <venkatasrisairameshmurala@gmail.com>
2024-07-02 13:00:00 -0400

    Add Input.jsx

---
+++ rag-assistant/frontend/src/components/common/Input.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Input = (props) => {
+  return (
+    <input style={{ padding: '0.5rem', flex: 1, border: '1px solid #ccc', borderRadius: '4px' }} {...props} />
+  )
+}
+
+export default Input
