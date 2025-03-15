926e51ce3ce111f1cb299c865703104750ef57a5
Ramesh Murala <venkatasrisairameshmurala@gmail.com>
2025-03-15 18:07:00 -0400

    Add main.py

---
+++ sentiment-analysis/backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from api.routers import sentiment
+from core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title=settings.PROJECT_NAME)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(sentiment.router, prefix="/api/v1")
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
